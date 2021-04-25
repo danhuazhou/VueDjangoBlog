d09475f48e7446a95fd0b70921a6d0d0cd13e54f
unknown <499428970@qq.com>
2021-04-26 01:55:23 +0800

    update blog views

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.pagination import PageNumberPagination
 
 
 # Create your views here.
+
+class BlogPagination(PageNumberPagination):
+    page_size = 10
+    page_query_param = 'page'
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 
 def hello(request):
     return HttpResponse("hello")
