9c968242b8676e65a7d8557f00412892aa422c7c
unknown <499428970@qq.com>
2023-06-20 18:36:22 +0800

    修改admin

--- backend/problems/admin.py
+++ backend/problems/admin.py
@@ -5,7 +5,18 @@ from .models import Problems, Tag, Category
 # Register your models here.
 
 class ProblemsAdmin(admin.ModelAdmin):
-    list_display = ("title",)
+    list_display = ("title", "difficulty", "emphasis", "passing_rate", "url")
+    search_fields = ("title",)
+
+    def passing_rate(self, obj):
+        s = int(obj.successes) if obj.successes else 0
+        f = int(obj.failtures) if obj.failtures else 0
+        if (s + f) != 0:
+            return format(s / (s + f), '.2%')
+        else:
+            return format(0, '.2%')
+
+    passing_rate.short_description = "通过率"
 
 
 class ProblemsCategoryAdmin(admin.ModelAdmin):
